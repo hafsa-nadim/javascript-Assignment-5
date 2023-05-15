4497285e9fab9bf0f22048b5b35ac07fef6e06ec
Sheikh Hafsa Nadeem <124384550+hafsa-nadim@users.noreply.github.com>
2023-05-15 23:47:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,202 @@
+                //      ************ASSIGNMENT#05*************
+
+
+    //   Question no:01
+
+//    var num1 = +prompt("Enter the first number:");
+//    var num2 = +prompt("Enter the second number:");
+
+//   var sum = num1+num2;
+//   document.write(" Sum of " + num1 + " and " + num2 + " is: " + sum);
+
+//       Question no:02
+    
+// For Subtraction:
+//    var num1 = prompt("Enter the first number:");
+//    var num2 = prompt("Enter the second number:");
+ 
+//    var Subtraction = num1 - num2;
+//   document.write(" Subtraction of " + num1 + " and " + num2 + " is: " + Subtraction);
+ 
+// For Multiplication:
+//    var num1 = prompt("Enter the first number:");
+//    var num2 = prompt("Enter the second number:");
+    
+//    var multiplication = num1 * num2;
+//    document.write(" Multiplication of " + num1 + " and " + num2 + " is: " + multiplication);
+    
+// For Division:
+    //   var num1 = prompt("Enter the first number:");
+    //   var num2 = prompt("Enter the second number:");
+
+    //   var division = num1/num2;
+    //   document.write("Division of " + num1 + " and " + num2 + " is: " + division);
+
+// For Modulus:
+        // var num1 = prompt("Enter the first number:");
+        // var num2 = prompt("Enter the second number:");
+
+        // var modulus = num1%num2;
+        // document.write("Modulus of " + num1 + " and " + num2 + " is: " + modulus);
+
+    //   Question no:03 
+
+// // a. Declare a variable.
+// var Variable;
+
+// // b. Show the value of the variable in your browser.
+// document.write("Value after variable declaration is: " + Variable + "<br>");
+
+// // c. Initialize the variable with some number.
+// Variable = 5;
+
+// // d. Show the value of the variable in your browser.
+// document.write("Initial value: " + Variable + "<br>");
+
+// // e. Increment the variable.
+// Variable++;
+
+// // f. Show the value of the variable in your browser.
+// document.write("Value after increment is: " + Variable + "<br>");
+
+// // g. Add 7 to the variable.
+// Variable += 7;
+
+// // h. Show the value of the variable in your browser.
+// document.write("Value after addition is: " + Variable + "<br>");
+
+// // i. Decrement the variable.
+// Variable--;
+
+// // j. Show the value of the variable in your browser.
+// document.write("Value after decrement is: " + Variable + "<br>");
+
+// // k. Show the remainder after dividing the variable's value by 3.
+// var remainder = Variable % 3;
+
+// // l. Show the remainder in your browser.
+// document.write("The remainder is: " + remainder + "<br>");
+
+    //  Question no:04
+
+// var price = 600;
+// var tickets = 5;
+// var totalCost = price * tickets;
+
+// console.log("The cost of buying " + tickets + " tickets is: " + totalCost + "PKR.");
+
+//      Question no:05
+
+// var num = +prompt("Enter a number:");
+// document.write("Table of " + num + "<br>");
+
+// for (var i = 1; i <= 10; i++) {
+//   var table = num * i;
+//   document.write(num + " x " + i + " = " + table + "<br>");
+// }
+
+//    Question no:06
+
+
+// var celsiusTemperature = 25;
+// var fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+// document.write(celsiusTemperature + "°C is " + fahrenheitTemperature + "°F<br>");
+
+// var fahrenheitTemperature2 = 70;
+// var celsiusTemperature2 = (fahrenheitTemperature2 - 32) * 5/9;
+// document.write(fahrenheitTemperature2 + "°F is " + celsiusTemperature2 + "°C");
+
+//      Question no: 07
+
+// var item1Price = 650;
+// var item2Price = 100;
+// var item1Quantity = 3;
+// var item2Quantity = 7;
+// var shippingCharges = 100;
+
+// var totalCost = (item1Price * item1Quantity) + (item2Price * item2Quantity) + shippingCharges;
+// document.write(" <h1>Shopping Cart</h1>");
+
+// document.write("<br> Price of item 1 is " + item1Price + "<br>");
+// document.write(" Quantity of item 1 is " + item1Quantity + "<br>");
+
+// document.write(" Price of item 2 is " + item2Price + "<br>");
+// document.write(" Quantity of item 2 is " + item2Quantity + "<br>");
+
+// document.write(" Shipping charges: " + shippingCharges + "<br><br>");
+// document.write(" Total cost of your order is " + totalCost);
+
+//      Question no: 08
+
+// var totalMarks = 980;
+// var marksObtained = 804;
+// var percentage = (marksObtained / totalMarks) * 100;
+
+// percentage = (percentage * 100) / 100;
+
+// document.write("<h1>Marks Sheet</h1>");
+// document.write("Total Marks: " + totalMarks + "<br>");
+// document.write("Marks Obtained: " + marksObtained + "<br>");
+// document.write("Percentage: " + percentage + "%");
+ 
+//        Question no: 09
+
+// var dollars = 10;
+// var riyals = 25;
+// var totalRupees = (dollars * 104.80) + (riyals * 28);
+
+// document.write("Total Currency in PKR : " + totalRupees);
+
+//         Question no: 10
+
+// var num = 7;
+// var result = ((num + 5) * 10) / 2;
+// document.write("Result: " + result);
+
+    //      Question no: 11
+
+// var currentYear = 2023;
+// var birthYear = 2005;
+// var age1 = currentYear - birthYear;
+
+// document.write("<h1>Age Calculator</h1>");
+// document.write("Current Year: " + currentYear + "<br>");
+// document.write("Birth Year: " + birthYear + "<br>");
+// document.write("Your age is " + age1 + " years old.");
+
+    //      Question no: 12
+
+// var radius = 20;
+// var circumference = 2 * 3.142 * radius;
+// var area = 3.142 * (radius * radius);
+
+// document.write("<h1>The Geometrizer</h1>");
+// document.write("Radius of the circle: " + radius + "<br>");
+// document.write("The circumference is: " + circumference + "<br>");
+// document.write("The area is: " + area);
+
+//          Question no: 13
+
+// var favoriteSnack = "chocolate chip";
+// var currentAge = 15;
+// var maximumAge = 65;
+// var amountPerDay = 3;
+
+// var yearsRemaining = maximumAge - currentAge;
+// var totalQuantity = amountPerDay * yearsRemaining * 365;
+
+// document.write("<h1>The Life Time Supply Calculator</h1>");
+// document.write("Favorite snack: " + favoriteSnack + "<br>");
+// document.write("Current age: " + currentAge + "<br>");
+// document.write("Estimated maximum age: " + maximumAge + "<br>");
+// document.write("Amount of snack per day: " + amountPerDay + "<br>");
+// document.write("You will need " + totalQuantity + " " + favoriteSnack + " to last you until the ripe old age of " + maximumAge + ".");
+
+
+
+
+
+
+
+
+
